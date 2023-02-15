8289bbccf25d60ccdd6674004aea520465d8defb
mknotts <71949985+mknotts@users.noreply.github.com>
2023-02-15 10:54:44 -0600

    Add files via upload

--- runParser.sh
+++ runParser.sh
@@ -1,6 +1,10 @@
+rm item.dat
+rm user.dat
+rm category.dat
+rm bid.dat
 python skeleton_parser.py ebay_data/items-*.json
-uniq item.dat
-uniq user.dat
-uniq category.dat
-uniq bid.dat
+uniq item.dat > item.dat
+uniq user.dat > user.dat
+uniq category.dat > category.dat
+uniq bid.dat > bid.dat
 
